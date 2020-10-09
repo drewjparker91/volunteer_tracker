76b1d96c4fc0cbd075e4238504ec0d6fb27c708b
drewparker <drewjparker91@gmail.com>
2020-10-09 12:28:18 -0700

    read integration specs, remove new_project.erb, update projects.erb to hold form to add a new project to /projects

--- app.rb
+++ app.rb
@@ -1,9 +1,10 @@
-require 'sinatra/reloader'
-require './lib/project'
-require './lib/volunteer'
-require 'pry'
-require 'pg'
-also_reload 'lib/**/*.rb'
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/project')
+require('./lib/volunteer')
+require('pry')
+require('pg')
+also_reload('lib/**/*.rb')
 
 DB = PG.connect({:dbname => "volunteer_tracker"})
 
@@ -27,20 +28,23 @@ get('/projects/:id/edit') do
 end
 
 post('/projects') do
-  name = params[:title]
+  title = params[:title]
   project = Project.new({:title => title, :id => nil})
   project.save()
-  redirect to('/projects')
+  @projects = Project.all
+  erb(:projects)
 end
 
 patch('/projects/:id') do
   @project = Project.find(params[:id].to_i())
   @project.update(params[:title])
+  @projects = Project.all
   redirect to('/projects')
 end
 
 delete('/projects/:id') do
   @project = Project.find(params[:id].to_i())
   @project.delete()
+  @projects = Project.all
   redirect to('/projects')
 end
