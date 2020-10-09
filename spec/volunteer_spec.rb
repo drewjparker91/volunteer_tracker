f9214c5759a6a28a9282f311c66212164f788fa1
drewparker <drewjparker91@gmail.com>
2020-10-09 11:02:58 -0700

    get good fail for #name in volunteer_spec.rb

--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -4,7 +4,7 @@ describe Volunteer do
   describe '#name' do
     it 'returns the name of the volunteer' do
       test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-      expect(test_volunteer.name).to eq 'Doe'
+      expect(test_volunteer.name).to eq 'Joan'
     end
   end
 
